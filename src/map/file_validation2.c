631e90e8534ae6c34e696c3d2aae4bd11334e5d6
AndreGomesSilva <silva_andregomes@outlook.com>
2024-07-02 18:24:59 -0300

    remove new line on check_elements

--- src/map/file_validation2.c
+++ src/map/file_validation2.c
@@ -6,11 +6,27 @@
 /*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:18:26 by angomes-          #+#    #+#             */
-/*   Updated: 2024/07/02 17:41:56 by iusantos         ###   ########.fr       */
+/*   Updated: 2024/07/02 18:23:43 by angomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+char	*gnl_wo_nl(int fd)
+{
+	char	*line;
+	char	*s;
+
+	line = get_next_line(fd);
+	if (line)
+	{
+		s = ft_strtrim(line, "\n");
+		free(line);
+		return (s);
+	}
+	return (NULL);
+}
+
 static int	ft_strcmp(const char *s1, const char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0')
@@ -23,9 +39,14 @@ static int	ft_strcmp(const char *s1, const char *s2)
 	return (*s1 - *s2);
 }
 
-static int store_if_valid_line(t_game *game, char *line)
+// static int  store_if_valid_ceiling_floor_line(t_game *game, char *line)
+// {
+//   
+// }
+
+static int	store_if_valid_texture_line(t_game *game, char *line)
 {
-	char **words;
+	char	**words;
 
 	words = ft_split(line, ' ');
 	if (words[2] != NULL)
@@ -35,15 +56,19 @@ static int store_if_valid_line(t_game *game, char *line)
 	}
 	if (words != NULL && words[0] != NULL && words[1] != NULL)
 	{
-		if (ft_strcmp(words[0], "NO") == 0 && game->wall_texture_path[NORTH] == NULL)
+		if (ft_strcmp(words[0], "NO") == 0
+			&& game->wall_texture_path[NORTH] == NULL)
 			game->wall_texture_path[NORTH] = ft_strdup(words[1]);
-		else if (ft_strcmp(words[0], "SO") == 0 && game->wall_texture_path[SOUTH] == NULL)
+		else if (ft_strcmp(words[0], "SO") == 0
+				&& game->wall_texture_path[SOUTH] == NULL)
 			game->wall_texture_path[SOUTH] = ft_strdup(words[1]);
-		else if (ft_strcmp(words[0], "EA") == 0 && game->wall_texture_path[EAST] == NULL)
+		else if (ft_strcmp(words[0], "EA") == 0
+				&& game->wall_texture_path[EAST] == NULL)
 			game->wall_texture_path[EAST] = ft_strdup(words[1]);
-		else if (ft_strcmp(words[0], "WE") == 0 && game->wall_texture_path[WEST] == NULL)
+		else if (ft_strcmp(words[0], "WE") == 0
+				&& game->wall_texture_path[WEST] == NULL)
 			game->wall_texture_path[WEST] = ft_strdup(words[1]);
-		else 
+		else
 		{
 			free_matrix(words);
 			return (print_error("Duplicated texture element in file.\n"));
@@ -53,21 +78,24 @@ static int store_if_valid_line(t_game *game, char *line)
 	return (E_OK);
 }
 
-int check_elements(t_game *game, char *file_name)
+int	check_elements(t_game *game, char *file_name)
 {
-  int fd;
-  char *line;
+	int		fd;
+	char	*line;
 
-  fd = open(file_name, O_RDONLY);
-  if (fd == - 1)
-    return (print_error("Could not open map file\n"));
-  while(TRUE)
-  {
-    line = get_next_line(fd);
-    if (!line)
-      break;
-    if(store_if_valid_line(game, line) == E_FAIL)
-		return (E_FAIL); 
-  } 
-  return (E_OK);
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+		return (print_error("Could not open map file\n"));
+	while (TRUE)
+	{
+		line = gnl_wo_nl(fd);
+		if (!line)
+			break ;
+		if (store_if_valid_texture_line(game, line) == E_FAIL)
+			return (E_FAIL);
+    // if (store_if_valid_ceiling_floor_line( game, line) == E_FAIL)
+      // return (E_FAIL);
+
+	}
+	return (E_OK);
 }

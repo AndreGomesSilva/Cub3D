d0b8969a2bedb6d842f5015f11273cc936c3d825
iury <iuryroberto@gmail.com>
2024-07-02 19:44:21 -0300

    WIP: next step is refactor

--- src/map/file_validation2.c
+++ src/map/file_validation2.c
@@ -6,7 +6,7 @@
 /*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:18:26 by angomes-          #+#    #+#             */
-/*   Updated: 2024/07/02 18:57:56 by angomes-         ###   ########.fr       */
+/*   Updated: 2024/07/02 19:38:11 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,35 +50,59 @@ static int parse_colors(t_game *game, char **rgb, char C_or_F)
   b = ft_atoi(rgb[2]);
   if ((r < 0 || g < 0 || b < 0) || (r > 255 || g > 255 || b > 255))
     return (print_error("Invalid color\n"));
-  if (C_or_F == 'C')
+  if (C_or_F == 'C' && game->ceiling.a != 255)
     game->ceiling.hex = get_hex_color(&game->ceiling, r, g, b);
-  else if (C_or_F == 'F')
+  else if (C_or_F == 'F' && game->floor.a != 255)
     game->floor.hex = get_hex_color(&game->floor, r, g, b);
+  else 
+	return (print_error("Floor or Ceiling setted more than once.\n"));
   return (E_OK);
+}
+
+static int	split_colors(t_game *game, char** words)
+{
+	char **rgb;
+
+	rgb = ft_split(words[1], ',');
+	if (rgb == NULL)
+	{
+		return (E_FAIL);
+	}
+	if (rgb[0] != NULL && rgb[1] != NULL && rgb[2] != NULL && rgb[3] == NULL)
+	{
+		if (parse_colors(game, rgb, words[0][0]) == E_FAIL)
+		{
+			free_matrix(rgb);
+			return (E_FAIL);
+		}
+		free_matrix(rgb);
+	}
+	return (E_OK);
 }
 
 static int  store_if_valid_ceiling_floor_line(t_game *game, char *line)
 {
-  static int *C_or_F; 
    char **words;
-   char **rgb;
 
   words = ft_split(line, ' ');
-  if (words[2] != NULL)
+  if (words != NULL && words[2] != NULL)
   {
     free_matrix(words);
     return (print_error("Badly formatted element line\n"));
   }
   if (words != NULL && words[0] != NULL && words[1] != NULL)
-  {
+  { 
     if (ft_strcmp(words[0], "F") == 0 || ft_strcmp(words[0], "C") == 0)
     {
-      rgb = ft_split(words[1], ',')
-      if (rgb[0] != NULL && rgb[1] != NULL && rgb[2] != NULL && rgb[3] == NULL)
-          parse_colors(game, rgb, words[0][0]);
-      free_matrix(rgb);
-    }
+		if (split_colors(game, words) == E_FAIL)
+			{
+				free_matrix(words);
+				return (E_FAIL);
+			}
+	}
+	free_matrix(words);
   }
+	return (E_OK);
 }
 
 static int	store_if_valid_texture_line(t_game *game, char *line)
@@ -130,8 +154,8 @@ int	check_elements(t_game *game, char *file_name)
 			break ;
 		if (store_if_valid_texture_line(game, line) == E_FAIL)
 			return (E_FAIL);
-    // if (store_if_valid_ceiling_floor_line( game, line) == E_FAIL)
-      // return (E_FAIL);
+		if (store_if_valid_ceiling_floor_line( game, line) == E_FAIL)
+			return (E_FAIL);
 
 	}
 	return (E_OK);

2ec8134a3af080a6651ab18774a5d7324df86d2b
AndreGomesSilva <silva_andregomes@outlook.com>
2024-07-03 18:31:32 -0300

    add: validated bg colors

---
+++ src/map/bg_validation.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bg_validation.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 17:31:03 by angomes-          #+#    #+#             */
+/*   Updated: 2024/07/03 18:25:48 by angomes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static int	rgb_is_number(char **rgb)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (rgb[i] != NULL)
+	{
+		j = 0;
+		while (rgb[i][j])
+		{
+			if (ft_isdigit(rgb[i][j]) == FALSE)
+				return (print_error("Color must be a non negative number\n"));
+			j++;
+		}
+		i++;
+	}
+	return (E_OK);
+}
+
+static int	parse_colors(t_color *bg_element, char **rgb)
+{
+	int	r;
+	int	g;
+	int	b;
+
+	if (rgb_is_number(rgb) == E_FAIL)
+		return (E_FAIL);
+	r = ft_atoi(rgb[0]);
+	g = ft_atoi(rgb[1]);
+	b = ft_atoi(rgb[2]);
+	if ((r < 0 || g < 0 || b < 0) || (r > 255 || g > 255 || b > 255))
+		return (print_error("Color must be a number between 0 and 255\n"));
+	bg_element->hex = get_hex_color(bg_element, r, g, b);
+	return (E_OK);
+}
+
+static int has_two_commas(char *line)
+{
+  int i;
+  int number_of_commas;
+
+  i = 0;
+  number_of_commas = 0;
+  while (line[i])
+  {
+    if (line[i] == ',')
+      number_of_commas++;
+    if (number_of_commas > 2)
+      return (print_error("badly formatted background colors\n"));
+    i++;
+  }
+  return (E_OK);
+}
+
+static int	set_floor_color(char *floor_content, t_color *floor)
+{
+	char	**rgb;
+
+  if (has_two_commas(floor_content) != E_OK)
+    return (E_FAIL);
+	rgb = ft_split(floor_content, ',');
+	if (rgb[0] != NULL && rgb[1] != NULL && rgb[2] != NULL && rgb[3] == NULL)
+  {
+		if (parse_colors(floor, rgb) != E_OK)
+    {
+      free_matrix(rgb);
+      return (E_FAIL);
+    }
+  }
+	else
+	{
+		free_matrix(rgb);
+		return (print_error("Badly formatted floor color \n"));
+	}
+	free_matrix(rgb);
+	return (E_OK);
+}
+
+static int	set_ceiling_color(char *ceiling_content, t_color *ceiling)
+{
+	char	**rgb;
+
+  if (has_two_commas(ceiling_content) != E_OK)
+    return (E_FAIL);
+	rgb = ft_split(ceiling_content, ',');
+	if (rgb[0] != NULL && rgb[1] != NULL && rgb[2] != NULL && rgb[3] == NULL)
+	{
+		if (parse_colors(ceiling, rgb) != E_OK)
+		{
+      free_matrix(rgb);
+      return (E_FAIL);
+		}
+	}
+	else
+	{
+		free_matrix(rgb);
+		return (print_error("Badly formatted ceiling color \n"));
+	}
+	free_matrix(rgb);
+	return (E_OK);
+}
+
+int	set_bg_colors(t_game *game)
+{
+	if (set_ceiling_color(game->file_content[C], &game->ceiling) != E_OK)
+		return (E_FAIL);
+	if (set_floor_color(game->file_content[F], &game->floor) != E_OK)
+		return (E_FAIL);
+	return (E_OK);
+}
--- src/map/file_validation2.c
+++ src/map/file_validation2.c
@@ -6,7 +6,7 @@
 /*   By: angomes- <angomes-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:18:26 by angomes-          #+#    #+#             */
-/*   Updated: 2024/07/03 16:07:00 by iusantos         ###   ########.fr       */
+/*   Updated: 2024/07/03 17:31:47 by angomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,51 +38,6 @@ static int	ft_strcmp(const char *s1, const char *s2)
 	}
 	return (*s1 - *s2);
 }
-//
-// static int	parse_colors(t_game *game, char **rgb, char C_or_F)
-// {
-// 	int	r;
-// 	int	g;
-// 	int	b;
-//
-// 	r = ft_atoi(rgb[0]);
-// 	g = ft_atoi(rgb[1]);
-// 	b = ft_atoi(rgb[2]);
-// 	if ((r < 0 || g < 0 || b < 0) || (r > 255 || g > 255 || b > 255))
-// 		return (print_error("Invalid color\n"));
-// 	if (C_or_F == 'C')
-// 		game->ceiling.hex = get_hex_color(&game->ceiling, r, g, b);
-// 	else if (C_or_F == 'F')
-// 		game->floor.hex = get_hex_color(&game->floor, r, g, b);
-// 	return (E_OK);
-// }
-//
-// static int	store_if_valid_ceiling_floor_line(t_game *game, char *line)
-// {
-// 	static int	*C_or_F;
-// 	char		**words;
-// 	char		**rgb;
-//
-// 	words = ft_split(line, ' ');
-// 	if (words[2] != NULL)
-// 	{
-// 		free_matrix(words);
-// 		return (print_error("Badly formatted element line\n"));
-// 	}
-// 	if (words != NULL && words[0] != NULL && words[1] != NULL)
-// 	{
-// 		if (ft_strcmp(words[0], "F") == 0 || ft_strcmp(words[0], "C") == 0)
-// 		{
-// 			rgb = ft_split(words[1], ',') if (rgb[0] != NULL && rgb[1] != NULL
-// 					&& rgb[2] != NULL && rgb[3] == NULL)
-// 				parse_colors(game, rgb, words[0][0]);
-// 			free_matrix(rgb);
-// 		}
-// 	}
-// }
-//
-//
-
 static int	store_elements(t_game *game, char **words)
 {
 	if (ft_strcmp(words[0], "NO") == 0 && game->file_content[NO] == NULL)
